99fe3f57289f8d32b2a22aa11b5799b2c44fcf9d
Vincent Fuertez <vfuertez@gmail.com>
2023-02-07 23:18:47 -0500

    h

--- landscaper.py
+++ landscaper.py
@@ -1,6 +1,6 @@
 
 
-work = {'tool': 0, 'cash': 0}
+work = {'tool': 0, 'bank': 0}
 
 equipment = [
     {"name": "teeth", "income": 1, "cost": 0},
@@ -10,7 +10,22 @@ equipment = [
     {"name": "team of starving students", "income": 250, "cost": 500},
 ]
 
-def do_work():
+def landscape():
     tool = equipment[work[tool]]
     print(f'You made an income of {equipment["income"]} using your {equipment["name"]}')
-    work["cash"] += equipment["income"]
+    work["bank"] += equipment["income"]
+    
+def status():
+    tool = equipment[work['tool']]
+    print(f"Your current equipment is {tool['name']} and have a bank account of {work['bank']} ")
+    
+def buy():
+    equipment_upgrade = equipment[work['tool']+1]
+    if (equipment_upgrade == None):
+        print("You can't upgrade anymore")
+        return 0
+    if (equipment_upgrade < equipment['cost']):
+        print("Not enough money")
+        return 0
+    if (work['bank'] < equipment_upgrade['cost']):
+        print('not enough money') 
